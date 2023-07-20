1c2ac84d2d06801c50fe68a11ac949aaf5de5230
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-07-20 17:41:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,40 @@ document.addEventListener('DOMContentLoaded', function () {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
             function (position) {
-                // ... (previously defined code)
+                // Get user's latitude, longitude, and altitude
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                const altitude = position.coords.altitude;
+
+                // Update user location in overlay
+                document.getElementById('user-location').innerText = `User Location: Latitude ${latitude}, Longitude ${longitude}`;
+
+                // Create and position the cube using three.js
+                const cube = new THREE.Mesh(
+                    new THREE.BoxGeometry(50, 50, 50),
+                    new THREE.MeshBasicMaterial({ color: 0xFF6347 })
+                );
+
+                const userPosition = new THREE.Vector3(longitude, altitude, -latitude);
+                const offsetDistance = 2;
+                const offsetVector = new THREE.Vector3(offsetDistance, 0, 0);
+                const cubePosition = userPosition.clone().add(offsetVector);
+                cube.position.set(cubePosition.x, 0, cubePosition.z);
+
+                scene.object3D.add(cube);
+
+                // Update box location in overlay
+                document.getElementById('box-location').innerText = `Box Location: Latitude ${latitude}, Longitude ${longitude} + ${offsetDistance} meters east`;
 
                 // Calculate azimuth (bearing) angle between user's location and the cube
                 const azimuth = calculateAzimuth(latitude, longitude, cubePosition.x, cubePosition.z);
-                document.getElementById('azimuth-angle').innerText = `Azimuth Angle: ${azimuth.toFixed(2)} degrees`;
+                document.getElementById('azimuth-angle').innerText = `Azimuth Angle: ${azimuth?.toFixed(2)} degrees`;
 
                 // Get device orientation (alpha value)
                 window.addEventListener('deviceorientation', function (event) {
                     const alpha = event.alpha;
                     // Update device direction in overlay
-                    document.getElementById('device-direction').innerText = `Device Direction: ${alpha.toFixed(2)} degrees`;
+                    document.getElementById('device-direction').innerText = `Device Direction: ${alpha?.toFixed(2)} degrees`;
 
                     // Add arrows only once after deviceorientation event starts
                     if (!arrowsAdded) {
@@ -57,6 +80,7 @@ function addArrows(deviceOrientation, azimuth) {
     // Add the arrows to the scene
     scene.object3D.add(arrowMesh);
 }
+
 
 // Function to calculate the azimuth angle between two coordinates
 function calculateAzimuth(lat1, lon1, lat2, lon2) {
