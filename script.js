e83afd508fd08a181ce136d4e5205860b2144507
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-07-20 16:34:53 +0300

    ..

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-// Wait for the document to load before initializing the AR.js scene
 document.addEventListener('DOMContentLoaded', function () {
-    // Get the AR.js scene
     const scene = document.querySelector('a-scene');
+    const overlayInfo = document.getElementById('overlay-info');
 
-    // Check for geolocation support
     if (navigator.geolocation) {
-        // Request the user's location
         navigator.geolocation.getCurrentPosition(
             function (position) {
-                // Get the user's latitude, longitude, and altitude
+                // Get user's latitude, longitude, and altitude
                 const latitude = position.coords.latitude;
                 const longitude = position.coords.longitude;
                 const altitude = position.coords.altitude;
 
-                // Create the cube using three.js
+                // Update user location in overlay
+                document.getElementById('user-location').innerText = `User Location: Latitude ${latitude}, Longitude ${longitude}`;
+
+                // Create and position the cube using three.js
                 const cube = new THREE.Mesh(
                     new THREE.BoxGeometry(0.5, 0.5, 0.5),
                     new THREE.MeshBasicMaterial({ color: 0xFF6347 })
                 );
 
-                // Calculate the position of the cube two meters to the east and on the ground
                 const userPosition = new THREE.Vector3(longitude, altitude, -latitude);
-                const offsetDistance = 2; // Two meters to the east
+                const offsetDistance = 2;
                 const offsetVector = new THREE.Vector3(offsetDistance, 0, 0);
                 const cubePosition = userPosition.clone().add(offsetVector);
                 cube.position.set(cubePosition.x, 0, cubePosition.z);
 
-                // Add the cube to the scene
                 scene.object3D.add(cube);
+
+                // Update box location in overlay
+                document.getElementById('box-location').innerText = `Box Location: Latitude ${latitude}, Longitude ${longitude} + ${offsetDistance} meters east`;
+
+                // Get device orientation (alpha value)
+                window.addEventListener('deviceorientation', function (event) {
+                    const alpha = event.alpha;
+                    // Update device direction in overlay
+                    document.getElementById('device-direction').innerText = `Device Direction: ${alpha.toFixed(2)} degrees`;
+                });
             },
             function (error) {
                 console.error('Error getting user location:', error);
